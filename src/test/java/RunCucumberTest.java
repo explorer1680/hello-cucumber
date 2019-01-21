400f5b89a096c5bcf2f53c17ad6b4a2fc0a2a6c0
echeng <echeng@ci.com>
2019-01-21 14:39:24 -0500

    add tags to test cases.

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -3,7 +3,12 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber-reports"})//, tags = "@smoke")
-//@CucumberOptions(plugin = {"pretty", "html:target/cucumber-reports"}, tags = "@smoke")
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber-reports"})
+//@CucumberOptions(
+//        features = "src/test/resources/selenium/selenium_tutorial_step1.feature",
+//        glue = {"selenium"},
+//        plugin = {"pretty", "html:target/cucumber-reports"},
+//        tags = "@smoke",
+//        monochrome = true)
 public class RunCucumberTest {
 }
